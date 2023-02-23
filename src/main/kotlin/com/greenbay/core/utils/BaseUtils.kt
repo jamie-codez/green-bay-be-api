048e37a3275e8c6b35c1e07b6764fcfbf8478611
Jamie Omondi <cruiseomondi90@gmail.com>
2023-02-23 22:13:46 +0300

    Added more boiler plate code

--- src/main/kotlin/com/greenbay/core/utils/BaseUtils.kt
+++ src/main/kotlin/com/greenbay/core/utils/BaseUtils.kt
@@ -1,7 +1,11 @@
 package com.greenbay.core.utils
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import io.vertx.core.json.JsonArray
 import io.vertx.core.json.JsonObject
+import io.vertx.ext.web.handler.JWTAuthHandler
+import java.util.*
 
 class BaseUtils {
     companion object {
@@ -13,5 +17,30 @@ class BaseUtils {
 
         fun getResponse(code: Int, message: String, payload: JsonArray): String =
             JsonObject.of("code", code, "message", message, "payload", payload).encodePrettily()
+
+        fun generateAccessJwt(email: String, roles: Array<String>): String {
+            return JWT.create().withSubject(email).withIssuer(System.getenv("ISSUER"))
+                .withArrayClaim("roles", roles)
+                .withExpiresAt(Date(System.currentTimeMillis() + (60 * 60 * 24 * 7 * 1000L)))
+                .withAudience(System.getenv("AUDIENCE")).withIssuedAt(Date(System.currentTimeMillis()))
+                .sign(Algorithm.HMAC256(System.getenv("JWT_SECRET")))
+        }
+
+        fun generateRefreshJwt(email: String, roles: Array<String>): String {
+            return JWT.create().withSubject(email).withIssuer(System.getenv("ISSUER"))
+                .withArrayClaim("roles", roles)
+                .withExpiresAt(Date(System.currentTimeMillis() + (60 * 60 * 24 * 30 * 1000L)))
+                .withAudience(System.getenv("AUDIENCE")).withIssuedAt(Date(System.currentTimeMillis()))
+                .sign(Algorithm.HMAC256(System.getenv("JWT_SECRET")))
+        }
+
+        fun verifyJwt(jwt: String): Boolean {
+            val decodedJwt = JWT.decode(jwt)
+            val verifier =
+                JWT.require(Algorithm.HMAC256(System.getenv("JWT_SECRET"))).withAudience(System.getenv("AUDIENCE"))
+                    .withIssuer(System.getenv("ISSUER")).build()
+            val decodedToken = verifier.verify(decodedJwt)
+            val payload = decodedJwt.payload.
+        }
     }
 }

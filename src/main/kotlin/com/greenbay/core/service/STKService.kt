b2de94cdd20166a555f326409b37aa1361fe8307
Jamie Omondi <crusiseomondi90@gmail.com>
2023-03-12 22:27:16 +0300

    Added epressService

---
+++ src/main/kotlin/com/greenbay/core/service/STKService.kt
@@ -0,0 +1,16 @@
+package com.greenbay.core.service
+
+import io.vertx.core.impl.logging.LoggerFactory
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.RoutingContext
+
+open class STKService:AuthService() {
+    private val logger = LoggerFactory.getLogger(this.javaClass.simpleName)
+    fun stSTKRoutes(router: Router){
+        router.post("/stk-push").handler(::stkPushExpress)
+        setAuthRoutes(router)
+    }
+    private fun stkPushExpress(rc:RoutingContext){
+
+    }
+}

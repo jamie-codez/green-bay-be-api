c8d4163b9901869f7b83901bc0be3bfd2fda32d9
jamie-codez <crusiseomondi90@gmail.com>
2023-03-14 23:13:52 +0300

    Working task service

--- src/main/kotlin/com/greenbay/core/service/TaskService.kt
+++ src/main/kotlin/com/greenbay/core/service/TaskService.kt
@@ -1,8 +1,16 @@
 package com.greenbay.core.service
 
+import com.greenbay.core.Collections
+import com.greenbay.core.TaskStatus
+import com.greenbay.core.utils.BaseUtils.Companion.execute
+import com.greenbay.core.utils.BaseUtils.Companion.getResponse
+import io.netty.handler.codec.http.HttpResponseStatus.CREATED
+import io.netty.handler.codec.http.HttpResponseStatus.INTERNAL_SERVER_ERROR
 import io.vertx.core.impl.logging.LoggerFactory
+import io.vertx.core.json.JsonArray
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
+import java.util.Date
 
 open class TaskService : CommunicationService() {
     private val logger = LoggerFactory.getLogger(this.javaClass.simpleName)
@@ -17,11 +25,29 @@ open class TaskService : CommunicationService() {
 
     private fun createTask(rc: RoutingContext) {
         logger.info("createTask() -->")
+        execute("createTask", rc, "admin", { user, body, response ->
+            body.put("createdBy", user.getString("email"))
+                .put("createdOn", Date(System.currentTimeMillis()))
+                .put("status", TaskStatus.PENDING.toString())
+            dbUtil.save(Collections.TASKS.toString(), body, {
+                response.end(getResponse(CREATED.code(), "Task created successfully"))
+            }, {
+                logger.error("createTask(${it.cause} -> creatingTask) <--")
+                response.end(getResponse(INTERNAL_SERVER_ERROR.code(), "Error occurred"))
+            })
+        }, "to", "title", "description", "scheduleDate")
         logger.info("createTask() <--")
     }
 
     private fun getTasks(rc: RoutingContext) {
         logger.info("getTasks() -->")
+        execute("getTasks", rc, "user", { user, body, response ->
+            val pageNumber = Integer.valueOf(rc.request().getParam("pageNumber")) - 1
+            val limit = 20
+            val skip = limit*pageNumber
+            val pipeline = JsonArray()
+
+        })
         logger.info("getTasks() <--")
     }
 
